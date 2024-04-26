ef05bde6a11fcf4f4b3c2bf0ee853397615e5370
Juwandito <juwanditorajendrawrahatnolo@gmail.com>
2024-04-26 09:49:39 +0700

    fungsi penambahan task DONE

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -13,9 +13,19 @@ const tasks = ref([
   // Tambahkan lebih banyak tasks jika diperlukan
 ]);
 
+// State untuk menyimpan nama task baru
+const newTaskName = ref('');
+
 // Fungsi untuk menghapus task
 function removeTask(taskId) {
   tasks.value = tasks.value.filter((task) => task.id !== taskId);
+}
+
+// Fungsi untuk menambahkan task
+function addTask() {
+  const newTask = { id: tasks.value.length + 1, name: newTaskName.value, completed: false };
+  tasks.value.push(newTask);
+  newTaskName.value = ''; // Reset nama task baru setelah ditambahkan
 }
 </script>
 
@@ -23,32 +33,24 @@ function removeTask(taskId) {
   <nav class="flex justify-between mx-8 items-center mb-10">
     <h2 class="text-xl font-medium text-slate-700">Hello</h2>
     <h1 class="text-4xl font-semibold text-slate-700">TaskDo</h1>
-    <RouterLink class="text-secondary font-medium" :to="{ name: 'index' }"
-      >Logout</RouterLink
-    >
+    <RouterLink class="text-secondary font-medium" :to="{ name: 'index' }">Logout</RouterLink>
   </nav>
   <div class="items-center text-center">
-    <button class="text-secondary mb-4">
+    <input type="text" v-model="newTaskName" placeholder="Enter new task name" />
+    <button class="text-secondary mb-4" @click="addTask">
       <FontAwesomeIcon :icon="faCirclePlus" /> Add new task
     </button>
-    <div>
-      <div
-        v-for="task in tasks"
-        :key="task.id"
-        class="flex justify-center items-center space-x-3"
-      >
-        <button class="text-slate-600 text-2xl" @click="removeTask(task.id)">
-          <FontAwesomeIcon :icon="faTrash" />
+    <div v-for="task in tasks" :key="task.id" class="flex justify-center items-center space-x-3">
+      <button class="text-slate-600 text-2xl" @click="removeTask(task.id)">
+        <FontAwesomeIcon :icon="faTrash" />
+      </button>
+      <RouterLink class="text-secondary font-medium" :to="{ name: 'edit' }">
+        <button class="text-slate-600 text-2xl">
+          <FontAwesomeIcon :icon="faPenToSquare" />
         </button>
-        <RouterLink class="text-secondary font-medium" :to="{ name: 'edit' }">
-          <button class="text-slate-600 text-2xl">
-            <FontAwesomeIcon :icon="faPenToSquare" />
-          </button>
-        </RouterLink>
-
-        <input type="checkbox" v-model="task.completed" class="checkbox" />
-        <span class="font-medium">{{ task.name }}</span>
-      </div>
+      </RouterLink>
+      <input type="checkbox" v-model="task.completed" class="checkbox" />
+      <span class="font-medium">{{ task.name }}</span>
     </div>
   </div>
 </template>
